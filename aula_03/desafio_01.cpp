758d60da3fadc7b121291766b8538f70c43c413d
joaopdalves <jhonstart.netalves@gmail.com>
2026-03-06 08:47:32 -0300

    Update desafio_01.cpp

--- aula_03/desafio_01.cpp
+++ aula_03/desafio_01.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int a[5];
+int main() {
+    int n
 	int soma = 0;
-	
-	for(int i = 0; i < 5; ++i){
-	cout << "informe um valor: ";
-		cin >> a[i];
-	
-	}
-	
-	for (int i = 0; i < 5; ++i) {
-        if (a[i] > 0){
-            soma += a[i];
-            
-		}
-			
-	}
-	
-	cout << "resultado: " << soma;
-	
-	return 0;
+
+    cout << "digite um numero inteiro positivo: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "por favor, digite um numero maior." << endl;
+    } else {
+        for (int i = 1; i <= n; i++) {
+            soma += i;
+        }
+
+        cout << "a soma dos numeros de 1 ate " << n << " eh: " << soma << endl;
+    }
+
+    return 0;
 }
