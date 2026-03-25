2bea35fca3b9c4c23d0978559244ec54ac45978a
joão pedro <jhonstart.netalves@gmail.com>
2026-03-25 11:17:51 -0300

    update22

---
+++ aula_06/desafio_03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool ehPar(int numero) {
+    return (numero % 2 == 0);
+}
+
+int main() {
+    int numero = 7;
+
+    if (ehPar(numero)) {
+        cout << numero << " é um número par." << endl;
+    } else {
+        cout << numero << " não é um número par." << endl;
+    }
+
+    return 0;
+}
